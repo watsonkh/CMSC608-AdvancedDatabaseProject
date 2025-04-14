9a6e542ec5707744e75d32471b4e631801cace66
watsonkh <watsonkh@vcu.edu>
2025-04-14 01:40:31 -0400

    updated fuzzy search query

--- flask/app.py
+++ flask/app.py
@@ -291,7 +291,7 @@ def recipe_search():
         embedding = create_text_embedding([query])[0].astype(float).tolist()
         conn = get_db_connection()
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute(f"SELECT id, name, mainimage, description from recipe INNER JOIN recipe_embeddings on recipe.id=recipe_embeddings.recipeid ORDER BY description_embedding <=> %s::vector DESC LIMIT 10", (embedding,))
+        cur.execute(f"SELECT id, name, mainimage, description from recipe INNER JOIN recipe_embeddings on recipe.id=recipe_embeddings.recipeid ORDER BY description_embedding <#> %s::vector ASC LIMIT 10", (embedding,))
         recipes = cur.fetchall()
         cur.close()
         conn.close()
@@ -304,7 +304,7 @@ def recipe_search():
         # Exact search:
         conn = get_db_connection()
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute(f"SELECT * from public.recipe WHERE LOWER(name) LIKE '%%' || LOWER(%s) || '%%' OR LOWER(description) LIKE '%%' || LOWER(%s) || '%%' ;", (query, query))
+        cur.execute(f"SELECT id, name, mainimage, description from recipe WHERE LOWER(name) LIKE '%%' || LOWER(%s) || '%%' OR LOWER(description) LIKE '%%' || LOWER(%s) || '%%' ;", (query, query))
         recipes = cur.fetchall()
         cur.close()
         conn.close()
